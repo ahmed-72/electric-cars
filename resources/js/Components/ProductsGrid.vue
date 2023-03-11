abb5a52fba051ccff75d276a2807c2115163b9a7
ahmed-72 <ahmadassar2001@gmail.com>
2023-03-12 01:33:07 +0200

    component

---
+++ resources/js/Components/ProductsGrid.vue
@@ -0,0 +1,40 @@
+<template>
+    <div v-for="product in products" class="p-6">
+        <img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="/images/squareCard.svg"
+            alt="blog">
+        <div class="inline-flex justify-between w-full">
+            <h1 class="mb-8 text-xl font-semibold leading-none tracking-tighter text-neutral-600">
+                {{ product.name }}
+            </h1>
+            <span>EUR {{ product.price }}</span>
+        </div>
+        <p class="mx-auto text-base font-medium leading-relaxed text-gray-500">
+            {{ product.rate }}
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ProductsGrid',
+
+    data() {
+        return {
+            products: []
+        }
+    },
+    methods: {
+        getProducts() {
+            axios.get('/api/products')
+                .then((response) => {
+                    this.products = response.data.data
+                })
+        }
+    },
+    mounted() {
+        this.getProducts()
+    }
+}
+
+</script>
