470a06c0502617c2bf687899f36b7c43de8a2fd2
ahmed-72 <ahmadassar2001@gmail.com>
2023-03-12 17:51:56 +0200

    requests

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric', 'min:0', 'max:100'],
+            'rate' => ['nullable', 'numeric', 'min:0', 'max:5'],
         ];
     }
 }
